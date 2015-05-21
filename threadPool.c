17b5cb0d5e00896e5ca6d667b540cb0ad9610116
Alex Bousso <alexbousso@gmail.com>
2015-05-21 20:53:04 +0300

    Tzoof is awesome! Shani too! Alex three!

--- threadPool.c
+++ threadPool.c
@@ -32,18 +32,28 @@ static Result wrapperToTask(ThreadPool* threadPool) {
 		return NULL_ARGUMENT;
 	}
 	
-	while (threadPool->destroyThreads) {
+	while (!threadPool->destroyThreads) {
 		TaskToRun *tsk;
 		
+		print("try to lock tasksQueueLock (wrapper)");
 		pthread_mutex_lock(&threadPool->tasksQueueLock);
+
+		
+		print("try to lock dontAddNewTaskLock (wrapper)");
 		pthread_mutex_lock(&threadPool->dontAddNewTaskLock);
+		print("dontAddNewTaskLock locked (wrapper)");
+		
 		while (threadPool->dontAddNewTasks == 0 && osIsQueueEmpty(threadPool->waitingTasks)) {
 			pthread_cond_wait(&(threadPool->DestroyIsOnOrTaskQNotEmpty), &(threadPool->tasksQueueLock));
 		}
+		print("tasksQueueLock locked (wrapper)");
 		if (threadPool->dontAddNewTasks && osIsQueueEmpty(threadPool->waitingTasks)) {
 			return SUCCESS;
 		}
+		
 		pthread_mutex_unlock(&threadPool->dontAddNewTaskLock);
+		print("dontAddNewTaskLock unlocked (wrapper)");
+		
 		//now: Q is not empty!
 		tsk = osDequeue(threadPool->waitingTasks);
 		if (!tsk) {
@@ -52,15 +62,17 @@ static Result wrapperToTask(ThreadPool* threadPool) {
 			return QUEUE_FAILURE;
 		}
 		pthread_mutex_unlock(&threadPool->tasksQueueLock);
+		print("tasksQueueLock unlocked (wrapper)");
 		
-		//Tzoof 
 		// Signals that the Queue is empty - for destroy.
-		if(!osIsQueueEmpty(threadPool->waitingTasks)){
+		if(osIsQueueEmpty(threadPool->waitingTasks)){
 			pthread_cond_signal(&(threadPool->TaskQueueEmpty));
+			print("DestroyLock signal sent  (wrapper)");
 		}
 		tsk->computeFunc(tsk->param);
 	}
 	
+	print("Quiting wrapper");
 	return SUCCESS;
 }
 
@@ -141,35 +153,51 @@ void tpDestroy(ThreadPool* threadPool, int shouldWaitForTasks) {
 	int i;
 	if (!threadPool) return;
 	
+	print("try to lock dontAddNewTaskLock (tpDestroy)");
 	pthread_mutex_lock(&threadPool->dontAddNewTaskLock);
+	print("dontAddNewTaskLock locked  (tpDestroy)");
+	
 	threadPool->dontAddNewTasks = 1;
+	
 	pthread_mutex_unlock(&threadPool->dontAddNewTaskLock);
+	print("dontAddNewTaskLock unlocked  (tpDestroy)");
 	
 	//if we shouldn't run all the waiting tasks in the Queue before destroy.
 	if (!shouldWaitForTasks) {
+	
+		print("try to lock tasksQueueLock (tpDestroy)");
 		pthread_mutex_lock(&threadPool->tasksQueueLock);
+		print("tasksQueueLock locked  (tpDestroy)");	
+		
 		while(!osIsQueueEmpty(threadPool->waitingTasks)) {
 			osDequeue(threadPool->waitingTasks);
 		}
+		
 		pthread_mutex_unlock(&threadPool->tasksQueueLock);
+		print("tasksQueueLock unlocked  (tpDestroy)");	
 	}
 	//TODO::Are we sure that we empty all the queue here?
 	
 	/*Checking that the task queue is empty - if we should not wait for that - we cleared the
 	Queue in the last paragraph */
+	
+	print("try to lock DestroyLock (tpDestroy)");
 	pthread_mutex_lock(&threadPool->DestroyLock);
 	while (!osIsQueueEmpty(threadPool->waitingTasks)) {
 		pthread_cond_wait(&(threadPool->TaskQueueEmpty),&threadPool->DestroyLock);
 	}
+	print("DestroyLock locked  (tpDestroy)");
+		
 	threadPool->destroyThreads = 1;
 	pthread_mutex_unlock(&threadPool->DestroyLock);
-
+	print("DestroyLock unlocked  (tpDestroy)");	
 	
 	// wait for threads to finish
 	for(i=0; i<threadPool->numberOfThreads; i++){
 		//Tzoof
 		//This signal is sent so that the wrapper won't wait for a new task when no task will be created;
 		pthread_cond_broadcast(&(threadPool->DestroyIsOnOrTaskQNotEmpty));
+		print("tasksQueueLock signal sent  (tpDestroy)");
 		pthread_join(threadPool->threadsIDs[i], NULL);
 	}
 	
@@ -197,7 +225,9 @@ int tpInsertTask(ThreadPool* threadPool, void (*computeFunc) (void *),
 		return -1;
 	}
 	
+	print("try to lock tasksQueueLock (tpInsertTask)");
 	pthread_mutex_lock(&(threadPool->tasksQueueLock));
+	print("tasksQueueLock locked  (tpInsertTask)");
 	
 	tsk->computeFunc = computeFunc;
 	tsk->param = param;
@@ -205,8 +235,9 @@ int tpInsertTask(ThreadPool* threadPool, void (*computeFunc) (void *),
 	assert(threadPool->waitingTasks);
 	osEnqueue(threadPool->waitingTasks, tsk);
 	pthread_mutex_unlock(&(threadPool->tasksQueueLock));
+	print("tasksQueueLock unlocked  (tpInsertTask)");
 	pthread_cond_signal(&(threadPool->DestroyIsOnOrTaskQNotEmpty));
-	
+	print("tasksQueueLock signal sent  (tpInsertTask)");
 	return 0;
 }
 
